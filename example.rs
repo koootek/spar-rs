7e5736d533044f89e77eff1e42ac24e730c4e855
koootek <190116302+koootek@users.noreply.github.com>
2026-02-03 16:36:09 +0100

    Update example

--- example.rs
+++ example.rs
@@ -5,15 +5,14 @@ fn main() {
     cnot::rebuild_edition(
         &mut std::env::args(),
         cnot::RustEdition::R2018,
-        "example.rs",
-        &["cnot.rs", "spar.rs"],
+        &["example.rs", "cnot.rs", "spar.rs"],
     );
     cnot::generate_project("example.rs", cnot::RustEdition::R2018).unwrap();
 
-    let bool_test = spar::flag_bool("bool", false);
-    let long_test = spar::flag_long("long", 0);
-    let ulong_test = spar::flag_ulong("ulong", 0);
-    let float_test = spar::flag_float("float", 0.0);
+    let bool_test   = spar::flag_bool("bool", false);
+    let long_test   = spar::flag_long("long", 0);
+    let ulong_test  = spar::flag_ulong("ulong", 0);
+    let float_test  = spar::flag_float("float", 0.0);
     let double_test = spar::flag_double("double", 0.0);
     let string_test = spar::flag_string("string", "");
     spar::parse_args(&mut std::env::args());
